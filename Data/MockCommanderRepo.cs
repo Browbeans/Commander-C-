a7b5cbab20491c82ff3204f869967282c185b421
Oliver Johansson <oliverjohansson3@gmail.com>
2021-09-05 11:40:46 +0200

    Added Boilerplate for predefined get requests

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command> 
+            {
+                new Command{Id = 0, HowTO="Boil an egg", Line="Boil water", Platform="Kettle and pan"}, 
+                new Command{Id = 1, HowTO="Cut Bread", Line="Get a knife", Platform="Choppingboard and knife"}, 
+                new Command{Id = 2, HowTO="Make tea", Line="Place teabag", Platform="Kettle and teabag"} 
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id = 0, HowTO="Boil an egg", Line="Boil water", Platform="Kettle and pan"};  
+        }
+    }
+}
